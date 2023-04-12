2a7e030638bd54fa22d31be738ed16f8d4cc5812
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-12 14:03:07 -0400

    set guess count equal to 0

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 secret_word = "amazedly"
 guess = ""
+guess_count = 0
 
 while guess != secret_word:
     guess = input("Enter guess: ")
 
 print("Congratulations, you win!")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
