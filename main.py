485913cd0e4a7ffdeab51ad3b8655274b0ee4ff8
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-12 18:03:49 -0400

    added : to line 7

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ guess_limit = 4
 guesses_limit_reached = False
 
 while guess != secret_word and not(guesses_limit_reached)):
-    if guess < guess_limit
+    if guess < guess_limit:
         guess = input("Enter guess: ")
         guess_count += 1
     else:
