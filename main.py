59ae698a3ed0fc1f26c29efe8a3e53d4031fb409
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-12 14:13:02 -0400

    set guess limit to 4

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 secret_word = "amazedly"
 guess = ""
 guess_count = 0
+guess_limit = 4
 
 while guess != secret_word:
     guess = input("Enter guess: ")
     guess_count += 1
-    
+
 print("Congratulations, you win!")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
