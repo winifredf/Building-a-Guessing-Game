11519a29f6665a72778df70de585a8317979d390
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-12 14:12:27 -0400

    Increment guess count by 1 each time it loops

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ guess_count = 0
 
 while guess != secret_word:
     guess = input("Enter guess: ")
-
+    guess_count += 1
+    
 print("Congratulations, you win!")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
