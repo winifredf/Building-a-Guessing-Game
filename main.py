15f2775c31775f92417e2b00d512fa51cbd0f165
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-12 17:55:55 -0400

    added if stmt limiting guesses

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ guess_limit = 4
 guesses_limit_reached = False
 
 while guess != secret_word:
-    guess = input("Enter guess: ")
-    guess_count += 1
+    if guess < guess_limit
+        guess = input("Enter guess: ")
+        guess_count += 1
 
 print("Congratulations, you win!")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
